af396c207321eaae81e77e26836c012238fdbbc4
Ian <ian.thomas@tiani-spirit.com>
2022-10-08 16:58:13 +0100

    modified:   go.mod 	modified:   go.sum 	modified:   tukxdw.go 	new file:   vendor/github.com/go-sql-driver/mysql/.gitignore 	new file:   vendor/github.com/go-sql-driver/mysql/AUTHORS 	new file:   vendor/github.com/go-sql-driver/mysql/CHANGELOG.md 	new file:   vendor/github.com/go-sql-driver/mysql/LICENSE 	new file:   vendor/github.com/go-sql-driver/mysql/README.md 	new file:   vendor/github.com/go-sql-driver/mysql/auth.go 	new file:   vendor/github.com/go-sql-driver/mysql/buffer.go 	new file:   vendor/github.com/go-sql-driver/mysql/collations.go 	new file:   vendor/github.com/go-sql-driver/mysql/conncheck.go 	new file:   vendor/github.com/go-sql-driver/mysql/conncheck_dummy.go 	new file:   vendor/github.com/go-sql-driver/mysql/connection.go 	new file:   vendor/github.com/go-sql-driver/mysql/connector.go 	new file:   vendor/github.com/go-sql-driver/mysql/const.go 	new file:   vendor/github.com/go-sql-driver/mysql/driver.go 	new file:   vendor/github.com/go-sql-driver/mysql/dsn.go 	new file:   vendor/github.com/go-sql-driver/mysql/errors.go 	new file:   vendor/github.com/go-sql-driver/mysql/fields.go 	new file:   vendor/github.com/go-sql-driver/mysql/fuzz.go 	new file:   vendor/github.com/go-sql-driver/mysql/infile.go 	new file:   vendor/github.com/go-sql-driver/mysql/nulltime.go 	new file:   vendor/github.com/go-sql-driver/mysql/nulltime_go113.go 	new file:   vendor/github.com/go-sql-driver/mysql/nulltime_legacy.go 	new file:   vendor/github.com/go-sql-driver/mysql/packets.go 	new file:   vendor/github.com/go-sql-driver/mysql/result.go 	new file:   vendor/github.com/go-sql-driver/mysql/rows.go 	new file:   vendor/github.com/go-sql-driver/mysql/statement.go 	new file:   vendor/github.com/go-sql-driver/mysql/transaction.go 	new file:   vendor/github.com/go-sql-driver/mysql/utils.go 	new file:   vendor/github.com/google/uuid/.travis.yml 	new file:   vendor/github.com/google/uuid/CONTRIBUTING.md 	new file:   vendor/github.com/google/uuid/CONTRIBUTORS 	new file:   vendor/github.com/google/uuid/LICENSE 	new file:   vendor/github.com/google/uuid/README.md 	new file:   vendor/github.com/google/uuid/dce.go 	new file:   vendor/github.com/google/uuid/doc.go 	new file:   vendor/github.com/google/uuid/hash.go 	new file:   vendor/github.com/google/uuid/marshal.go 	new file:   vendor/github.com/google/uuid/node.go 	new file:   vendor/github.com/google/uuid/node_js.go 	new file:   vendor/github.com/google/uuid/node_net.go 	new file:   vendor/github.com/google/uuid/null.go 	new file:   vendor/github.com/google/uuid/sql.go 	new file:   vendor/github.com/google/uuid/time.go 	new file:   vendor/github.com/google/uuid/util.go 	new file:   vendor/github.com/google/uuid/uuid.go 	new file:   vendor/github.com/google/uuid/version1.go 	new file:   vendor/github.com/google/uuid/version4.go 	new file:   vendor/github.com/ipthomas/tukcnst/LICENSE 	new file:   vendor/github.com/ipthomas/tukcnst/README.md 	new file:   vendor/github.com/ipthomas/tukcnst/tukcnst.go 	new file:   vendor/github.com/ipthomas/tukdbint/LICENSE 	new file:   vendor/github.com/ipthomas/tukdbint/README.md 	new file:   vendor/github.com/ipthomas/tukdbint/tukdbint.go 	new file:   vendor/github.com/ipthomas/tukdsub/LICENSE 	new file:   vendor/github.com/ipthomas/tukdsub/README.md 	new file:   vendor/github.com/ipthomas/tukdsub/tukdsub.go 	new file:   vendor/github.com/ipthomas/tukhttp/LICENSE 	new file:   vendor/github.com/ipthomas/tukhttp/README.md 	new file:   vendor/github.com/ipthomas/tukhttp/tukhttp.go 	new file:   vendor/github.com/ipthomas/tukpdq/.DS_Store 	new file:   vendor/github.com/ipthomas/tukpdq/LICENSE 	new file:   vendor/github.com/ipthomas/tukpdq/README.md 	new file:   vendor/github.com/ipthomas/tukpdq/tukpdq.go 	new file:   vendor/github.com/ipthomas/tukutil/README.md 	new file:   vendor/github.com/ipthomas/tukutil/tukutil.go 	new file:   vendor/modules.txt

--- go.mod
+++ go.mod
@@ -2,9 +2,16 @@ module github.com/ipthomas/tukxdw
 
 go 1.19
 
-require github.com/ipthomas/tukutil v1.2.5
+require (
+	github.com/ipthomas/tukcnst v1.2.13
+	github.com/ipthomas/tukdbint v1.2.6
+	github.com/ipthomas/tukdsub v1.2.14
+)
 
 require (
+	github.com/ipthomas/tukutil v1.2.5 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/ipthomas/tukcnst v1.2.12 // indirect
+	github.com/ipthomas/tukhttp v1.2.11 // indirect
+	github.com/ipthomas/tukpdq v1.2.29 // indirect
 )
--- tukxdw.go
+++ tukxdw.go
@@ -1,22 +1,16 @@
 package tukxdw
 
 import (
-	"bytes"
 	"encoding/json"
 	"encoding/xml"
-	"errors"
-	"fmt"
-	"html/template"
-	"io"
 	"log"
-	"net/http"
 	"os"
-	"strings"
-	"tukcnst"
-	"tukdb-readwriter/tuk"
-	"tukdbint"
 
-	"github.com/ipthomas/tukutil"
+	"github.com/ipthomas/tukdbint"
+
+	"github.com/ipthomas/tukcnst"
+
+	"github.com/ipthomas/tukdsub"
 )
 
 type DSUBSubscribeResponse struct {
@@ -88,42 +82,61 @@ type WorkflowDefinition struct {
 		} `json:"output,omitempty"`
 	} `json:"tasks"`
 }
+type XDWTransaction struct {
+	Action             string
+	Pathway            string
+	WorkflowDefinition WorkflowDefinition
+	DSUB_BrokerURL     string
+	DSUB_ConsumerURL   string
+}
+type XDW_Int interface {
+	processRequest() error
+}
 
 var (
-	TUK_DB_URL        = ""
 	DSUB_BROKER_URL   = ""
 	DSUB_CONSUMER_URL = ""
 )
 
-func InitOSvars() {
-	TUK_DB_URL = os.Getenv("TUK_DB_URL")
+func init() {
 	DSUB_BROKER_URL = os.Getenv("DSUB_BROKER_URL")
 	DSUB_CONSUMER_URL = os.Getenv("DSUB_CONSUMER_URL")
-
-	if TUK_DB_URL == "" {
-		TUK_DB_URL = "https://5k2o64mwt5.execute-api.eu-west-1.amazonaws.com/beta/"
-	}
-	if DSUB_CONSUMER_URL == "" {
-		DSUB_CONSUMER_URL = "https://cjrvrddgdh.execute-api.eu-west-1.amazonaws.com/beta/"
-	}
-	if DSUB_BROKER_URL == "" {
-		DSUB_BROKER_URL = "http://spirit-test-01.tianispirit.co.uk:8081/SpiritXDSDsub/Dsub"
-	}
 }
-func RegisterWorkflowDefinition(wfdef string) error {
-	//[pwy]pwyExpressions
-	brokerExps := make(map[string]map[string]string)
-	xdwdefs := make(map[string]WorkflowDefinition)
-	if xdwdef, updated := updateXDWDefinition(wfdef); updated {
-		xdwdefs[xdwdef.Ref] = xdwdef
-		//[expression]pathway
-		pwyExpressions := make(map[string]string)
+
+// Convienance method to obtain initialised WorkflowDefinition struct which will be needed in the your tukxdw.New_Transaction(&XDWTransaction) if i.Action='register'
+func NewWorkflowDefinition(wfdefstring string, wfdefbytes []byte) (WorkflowDefinition, error) {
+	wfdefstruct := WorkflowDefinition{}
+	if wfdefbytes == nil {
+		wfdefbytes = []byte(wfdefstring)
+	}
+	err := json.Unmarshal(wfdefbytes, &wfdefstruct)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return wfdefstruct, err
+}
+func New_Transaction(i XDW_Int) error {
+	return i.processRequest()
+}
+func (i *XDWTransaction) processRequest() error {
+	switch i.Action {
+	case tukcnst.REGISTER:
+		return i.RegisterWorkflowDefinition()
+	case tukcnst.XDW_CONTENT_CREATOR:
+	case tukcnst.XDW_CONTENT_CONSUMER:
+	case tukcnst.XDW_CONTENT_UPDATER:
+	}
+	return nil
+}
+func (i *XDWTransaction) RegisterWorkflowDefinition() error {
+	pwyExpressions := make(map[string]string)
+	if err := i.PersistXDWDefinition(); err == nil {
 		log.Println("Parsing XDW Tasks for potential DSUB Broker Subscriptions")
-		for _, task := range xdwdef.Tasks {
+		for _, task := range i.WorkflowDefinition.Tasks {
 			for _, inp := range task.Input {
 				log.Printf("Checking Input Task %s", inp.Name)
 				if inp.AccessType == tukcnst.XDS_REGISTERED {
-					pwyExpressions[inp.Name] = xdwdef.Ref
+					pwyExpressions[inp.Name] = i.Pathway
 					log.Printf("Task %v %s task input %s included in potential DSUB Broker subscriptions", task.ID, task.Name, inp.Name)
 				} else {
 					log.Printf("Input Task %s does not require a dsub broker subscription", inp.Name)
@@ -132,175 +145,45 @@ func RegisterWorkflowDefinition(wfdef string) error {
 			for _, out := range task.Output {
 				log.Printf("Checking Output Task %s", out.Name)
 				if out.AccessType == tukcnst.XDS_REGISTERED {
-					pwyExpressions[out.Name] = xdwdef.Ref
+					pwyExpressions[out.Name] = i.Pathway
 					log.Printf("Task %v %s task output %s included in potential DSUB Broker subscriptions", task.ID, task.Name, out.Name)
 				} else {
 					log.Printf("Output Task %s does not require a dsub broker subscription", out.Name)
 				}
 			}
 		}
-		brokerExps[xdwdef.Ref] = pwyExpressions
 	}
-	log.Printf("Found %v potential DSUB Broker Subscriptions - %s", len(brokerExps), brokerExps)
-	for pwy, pwyExpressions := range brokerExps {
-		sub := tukdbint.Subscription{Pathway: strings.ToUpper(pwy)}
-		subs := tukdbint.Subscriptions{Action: "delete"}
-		subs.Subscriptions = append(subs.Subscriptions, sub)
-		if err := tukdbint.NewDBEvent(&subs); err != nil {
-			log.Println(err.Error())
-			return err
+	log.Printf("Found %v potential DSUB Broker Subscriptions - %s", len(pwyExpressions), pwyExpressions)
+	if len(pwyExpressions) > 0 {
+		event := tukdsub.DSUBEvent{Action: tukcnst.CANCEL, Pathway: i.Pathway}
+		tukdsub.New_Transaction(&event)
+		event.Action = tukcnst.CREATE
+		for expression := range pwyExpressions {
+			event.Expressions = append(event.Expressions, expression)
 		}
-		for exp, pwy := range pwyExpressions {
-			subscribe := DSUBSubscribe{
-				Topic:      tukcnst.DSUB_TOPIC_TYPE_CODE,
-				Expression: exp,
-			}
-			if err := subscribe.NewEvent(); err != nil {
-				log.Println(err.Error())
-				return err
-			}
-			tuksub := tukdbint.Subscription{
-				BrokerRef:  subscribe.BrokerRef,
-				Pathway:    strings.ToUpper(pwy),
-				Topic:      tukcnst.DSUB_TOPIC_TYPE_CODE,
-				Expression: exp,
-			}
-			tuksubs := tukdbint.Subscriptions{
-				Action: "insert",
-			}
-			tuksubs.Subscriptions = append(tuksubs.Subscriptions, tuksub)
-			if err := tukdbint.NewDBEvent(&tuksubs); err != nil {
-				log.Println(err.Error())
-				return err
-			}
-		}
+		tukdsub.New_Transaction(&event)
 	}
 	return nil
 }
-func (i *DSUBSubscribe) NewEvent() error {
-	i.UUID = tukutil.NewUuid()
-	i.BrokerUrl = DSUB_BROKER_URL
-	i.ConsumerUrl = DSUB_CONSUMER_URL
-	reqMsg := "{{define \"subscribe\"}}<SOAP-ENV:Envelope xmlns:SOAP-ENV='http://www.w3.org/2003/05/soap-envelope' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:s='http://www.w3.org/2001/XMLSchema' xmlns:wsa='http://www.w3.org/2005/08/addressing'><SOAP-ENV:Header><wsa:Action SOAP-ENV:mustUnderstand='true'>http://docs.oasis-open.org/wsn/bw-2/NotificationProducer/SubscribeRequest</wsa:Action><wsa:MessageID>urn:uuid:{{.UUID}}</wsa:MessageID><wsa:ReplyTo SOAP-ENV:mustUnderstand='true'><wsa:Address>http://www.w3.org/2005/08/addressing/anonymous</wsa:Address></wsa:ReplyTo><wsa:To>{{.BrokerUrl}}</wsa:To></SOAP-ENV:Header><SOAP-ENV:Body><wsnt:Subscribe xmlns:wsnt='http://docs.oasis-open.org/wsn/b-2' xmlns:a='http://www.w3.org/2005/08/addressing' xmlns:rim='urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0' xmlns:wsa='http://www.w3.org/2005/08/addressing'><wsnt:ConsumerReference><wsa:Address>{{.ConsumerUrl}}</wsa:Address></wsnt:ConsumerReference><wsnt:Filter><wsnt:TopicExpression Dialect='http://docs.oasis-open.org/wsn/t-1/TopicExpression/Simple'>ihe:FullDocumentEntry</wsnt:TopicExpression><rim:AdhocQuery id='urn:uuid:742790e0-aba6-43d6-9f1f-e43ed9790b79'><rim:Slot name='{{.Topic}}'><rim:ValueList><rim:Value>('{{.Expression}}')</rim:Value></rim:ValueList></rim:Slot></rim:AdhocQuery></wsnt:Filter></wsnt:Subscribe></SOAP-ENV:Body></SOAP-ENV:Envelope>{{end}}"
-	tmplt, err := template.New(tukcnst.SUBSCRIBE).Parse(reqMsg)
-	if err != nil {
+func (i *XDWTransaction) PersistXDWDefinition() error {
+	log.Println("Processing WF Def for Pathway : " + i.Pathway)
+	xdw := tukdbint.XDW{Name: i.Pathway}
+	xdws := tukdbint.XDWS{Action: tukcnst.DELETE}
+	xdws.XDW = append(xdws.XDW, xdw)
+	if err := tukdbint.NewDBEvent(&xdws); err != nil {
 		log.Println(err.Error())
 		return err
 	}
-	var b bytes.Buffer
-	err = tmplt.Execute(&b, i)
-	if err != nil {
-		log.Println(err.Error())
-		return err
-	}
-	i.Request = b.Bytes()
-	err = i.createSubscription()
-	if err != nil {
-		log.Println(err.Error())
-	}
-	return err
-}
-func (i *DSUBSubscribe) createSubscription() error {
-	log.Printf("Sending DSUB Subscribe request to %s", i.BrokerUrl)
-	reqStr := string(i.Request)
-	log.Println(reqStr)
-	var resp *http.Response
-	req, err := http.NewRequest(http.MethodPost, i.BrokerUrl, strings.NewReader(reqStr))
-	if err != nil {
-		log.Println(err.Error())
-		return err
-	}
-	req.Header.Set(tukcnst.SOAP_ACTION, tukcnst.SOAP_ACTION_SUBSCRIBE_REQUEST)
-	req.Header.Set(tukcnst.CONTENT_TYPE, tukcnst.SOAP_XML)
-	req.Header.Set(tukcnst.ACCEPT, tukcnst.ALL)
-	req.Header.Set(tukcnst.CONNECTION, tukcnst.KEEP_ALIVE)
-	resp, err = http.DefaultClient.Do(req)
-	if err != nil {
-		log.Println(err.Error())
-		return err
-	}
-	rsp, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Println(err.Error())
-		return err
-	}
-	subrsp := DSUBSubscribeResponse{}
-	log.Println("DSUB Broker Response")
-	log.Println(string(rsp))
-	if err := xml.Unmarshal(rsp, &subrsp); err != nil {
-		log.Println(err.Error())
-	}
-	i.BrokerRef = subrsp.Body.SubscribeResponse.SubscriptionReference.Address
-	if i.BrokerRef == "" {
-		log.Println("No Broker Reference found in response message from DSUB Broker")
-		err = errors.New("no broker reference found in response message from dsub broker")
-	} else {
-		log.Printf("Broker Response. Broker Ref :  %s", subrsp.Body.SubscribeResponse.SubscriptionReference.Address)
-	}
-	return err
-}
-func updateXDWDefinition(wfdef string) (WorkflowDefinition, bool) {
-	xdwdef := WorkflowDefinition{}
-	xdwfile, err := os.Open(Basepath + "/xdwconfig/" + file.Name())
-	if err != nil {
-		log.Println(err.Error())
-		return xdwdef, false
-	}
-	json.NewDecoder(xdwfile).Decode(&xdwdef)
-	log.Println("Loaded WF Def for Pathway : " + xdwdef.Ref)
+	log.Printf("Deleted Existing XDW Definition for Pathway %s", i.Pathway)
 
-	xdw := tuk.XDW{Name: xdwdef.Ref}
-	xdws := tuk.XDWS{Action: tukcnst.DELETE}
+	xdwBytes, _ := json.Marshal(i.WorkflowDefinition)
+	xdw = tukdbint.XDW{Name: i.Pathway, IsXDSMeta: false, XDW: string(xdwBytes)}
+	xdws = tukdbint.XDWS{Action: tukcnst.INSERT}
 	xdws.XDW = append(xdws.XDW, xdw)
-	if err := xdws.NewEvent(); err != nil {
+	if err := tukdbint.NewDBEvent(&xdws); err != nil {
 		log.Println(err.Error())
-		return xdwdef, false
+		return err
 	}
-	xdwBytes, _ := json.Marshal(xdwdef)
-	xdw = tuk.XDW{Name: xdwdef.Ref, IsXDSMeta: false, XDW: string(xdwBytes)}
-	xdws = tuk.XDWS{Action: tukcnst.INSERT}
-	xdws.XDW = append(xdws.XDW, xdw)
-	if err := xdws.NewEvent(); err != nil {
-		log.Println(err.Error())
-		return xdwdef, false
-	}
-	return xdwdef, true
-}
-func (i *Subscriptions) NewEvent() error {
-	log.Printf("Sending Request to %s", TUK_DB_URL+tukcnst.SUBSCRIPTIONS)
-	body, _ := json.Marshal(i)
-	bodyBytes, err := newTUKDBRequest(tukcnst.SUBSCRIPTIONS, body)
-	if err == nil {
-		if err := json.Unmarshal(bodyBytes, &i); err != nil {
-			fmt.Println(err.Error())
-		}
-	}
-	return err
-}
-func newTUKDBRequest(resource string, body []byte) ([]byte, error) {
-	if TUK_DB_URL == "" {
-		TUK_DB_URL = "https://5k2o64mwt5.execute-api.eu-west-1.amazonaws.com/beta/"
-	}
-	client := &http.Client{}
-	req, err := http.NewRequest(http.MethodPost, TUK_DB_URL+resource, bytes.NewBuffer(body))
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-	req.Header.Add(tukcnst.CONTENT_TYPE, tukcnst.APPLICATION_JSON_CHARSET_UTF_8)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-	log.Printf("Response Status Code %v\n", resp.StatusCode)
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := io.ReadAll(resp.Body)
-		if err != nil {
-			log.Println(err)
-		} else {
-			return bodyBytes, nil
-		}
-	}
-	return nil, err
+	log.Printf("Persisted New XDW Definition for Pathway %s", i.Pathway)
+	return nil
 }
