e42324aadee752a4d5330a381c19616447ea61d6
Ian <ian.thomas@tiani-spirit.com>
2023-05-12 12:46:23 +0100

    modified:   tukxdw.go

--- tukxdw.go
+++ tukxdw.go
@@ -701,7 +701,7 @@ func (i *Transaction) ContentConsumer() error {
 		return err
 	}
 	i.XDWEvents = tukdbint.Events{Action: tukcnst.SELECT}
-	ev := tukdbint.Event{Pathway: i.Pathway, NhsId: i.NHS_ID, Version: i.XDWVersion}
+	ev := tukdbint.Event{Pathway: i.Pathway, NhsId: i.NHS_ID, Version: i.XDWVersion, TaskId: -1}
 	i.XDWEvents.Events = append(i.XDWEvents.Events, ev)
 	if err := tukdbint.NewDBEvent(&i.XDWEvents); err != nil {
 		log.Println(err.Error())
